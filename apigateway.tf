e251bd5fe75c972110bab708d512a4deb3ed90ef
ane4ka0205 <aamalanova@gmail.com>
2019-01-16 16:33:36 +0000

    updated

--- apigateway.tf
+++ apigateway.tf
@@ -7,4 +7,17 @@ resource "aws_api_gateway_rest_api" "mygw" {
   }
 }
 
+resource "aws_api_gateway_resource" "Myresource"{
+  rest_api_d = "${aws_api_gateway_rest_api.myge.id}"
+  parent_id = "${aws_api_gateway_rest_api.mygw.root_resource_id}"
+  path_part = "myresource"
+}
+
+resource "aws_api_gateway_method" "Mymethod" {
+  rest_api_id = "${aws_api_gateway_rest_api.mygw.id}"
+  resource_id = "${aws_api_gateway_resource.Myresource.id}"
+  http_method = "GET"
+  authorization = "NONE"
+}
+
 
