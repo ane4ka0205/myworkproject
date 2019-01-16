61d3df016653a930efeca37f317bc6beb169d606
ane4ka0205 <aamalanova@gmail.com>
2019-01-16 17:01:29 +0000

    updated gateway

--- apigateway.tf
+++ apigateway.tf
@@ -20,4 +20,20 @@ resource "aws_api_gateway_method" "Mymethod" {
   authorization = "NONE"
 }
 
+resource "aws_api_gateway_intergration" "Myintegration" {
+  rest_api_id = "${aws_api_gateway_rest_api.mygw.id}"
+  resource_id = "${aws_api_gateway_resource.Myresource.id}"
+  http_method = "${aws_api_gateway_method.Mymethod.http_method}"
+  type = "MOCK"
+}
 
+resource "aws_api_gateway_deployment" "Mydeploy" {
+  depends_on = ["aws_api_gateway_integration.Myintegration"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.mygw.id}"
+  stage_name = "dev"
+  
+  variables = {
+    "answer" = "42"
+  }
+}
